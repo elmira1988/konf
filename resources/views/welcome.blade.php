1c269a43b4d0210b82179628eca7594ce92a6dc0
elmira1988 <elmira.sharapova@yandex.ru>
2021-05-19 21:42:28 +0500

    Поменяла фон

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,87 +9,27 @@
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
         <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
     </head>
     <body>
         <div class="flex-center position-ref full-height" id="app">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
+            <div class="container content">
+                <div class="m-b-md mt-5">
+                    <h3 class="text-center">
+                    IX Международная научная коференция
+                        <br><b>«Современные проблемы математики и физики»</b>
+                    <br>посвященная 70-летию чл.-корр. АН РБ К.Б. Сабитова
+                    </h3>
+                    <h5 class="text-center">12 – 15 сентября 2021 года</h5>
 
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="m-b-md">
-                    <h4>
-                    IX Международная научная коференция  «Современные проблемы математики и физики»
-                    <br>
-                        посвященная 70-летию чл.-корр. АН РБ К.Б. Сабитова
-                    <br>12 – 15 сентября 2021 года
-                    </h4>
+                    <div class="d-flex justify-content-around">
+                            <button type="button" class="btn btn-link">ОРГКОМИТЕТ</button>
+                            <button type="button" class="btn btn-link">ПРОГРАММНЫЙ КОМИТЕТ</button>
+                            <button type="button" class="btn btn-link">ФОРМЫ УЧАСТИЯ</button>
+                            <button type="button" class="btn btn-link">ПРАВИЛА ОФОРМЛЕНИЯ ТЕЗИСОВ</button>
+                    </div>
                 </div>
 
-                <h5>
+                <h5 class="mt-5">
                     СЕКЦИИ КОНФЕРЕНЦИИ
                 </h5>
                 <ol class="text-left">
