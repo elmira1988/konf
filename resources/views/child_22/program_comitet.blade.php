320bf46d5d1466bbfe0ebc33db5905d9cb630112
Sergey Bulankin <sergeybulankin@gmail.com>
2022-03-22 16:37:46 +0500

    Обновлена информация о метаданных и коммитетах

--- resources/views/child_22/program_comitet.blade.php
+++ resources/views/child_22/program_comitet.blade.php
@@ -9,88 +9,87 @@
                 </button>
             </div>
             <div class="modal-body">
-
-                {{--<div class="row mt-3">
-                    <div class="col-lg-3"> <h6 class="font-weight-bold">Нигматулин Р.И.</h6></div>
-                    <div class="col-lg-3"> <h6>сопредседатель</h6> </div>
-                    <div class="col-lg-6"> <h6>научный руководитель Института океанологии <br>им. П.П. Ширшова РАН, академик РАН, г. Москва</h6></div>
+                <div class="row">
+                    <div class="col-lg-3"> <h6 class="font-weight-bold">Алёшин П.Н.</h6></div>
+                    <div class="col-lg-3"> <h6>к.и.н., доцент</h6></div>
+                    <div class="col-lg-6"> <h6>Заместитель директора по научной работе СФ БашГУ</h6></div>
                 </div>
                 <hr class="m-1">
 
-                <div class="row mt-3">
-                    <div class="col-lg-3"> <h6 class="font-weight-bold"> Моисеев Е.И.</h6></div>
-                    <div class="col-lg-3"> <h6>сопредседатель</h6> </div>
-                    <div class="col-lg-6"> <h6>президент факультета ВМК МГУ, <br> академик РАН, г. Москва</h6></div>
+                <div class="row">
+                    <div class="col-lg-3"> <h6 class="font-weight-bold">Акилов Д.З.</h6></div>
+                    <div class="col-lg-3"> </div>
+                    <div class="col-lg-6"> <h6>Заместитель главы Куюргазинского района по социальной политике</h6></div>
                 </div>
                 <hr class="m-1">
 
-                <div class="row mt-3">
-                    <div class="col-lg-3"> <h6 class="font-weight-bold"> Сабитов К.Б.</h6></div>
-                    <div class="col-lg-3"> <h6>зам. председателя</h6> </div>
-                    <div class="col-lg-6"> <h6> чл.-корр. АН РБ, г. Стерлитамак</h6></div>
+                <div class="row">
+                    <div class="col-lg-3"> <h6 class="font-weight-bold">Михайлова Л.И.</h6></div>
+                    <div class="col-lg-3"> </div>
+                    <div class="col-lg-6"> <h6>Руководитель попечительского совета благотворительного фонда «Свидетель Милосердия»</h6></div>
                 </div>
                 <hr class="m-1">
 
+                <div class="row">
+                    <div class="col-lg-3"> <h6 class="font-weight-bold">Нафикова А.И.</h6></div>
+                    <div class="col-lg-3"> <h6>доцент</h6></div>
+                    <div class="col-lg-6"> <h6>Кандидат философских наук СФ БашГУ</h6></div>
+                </div>
+                <hr class="m-1">
 
-                <div class="row mt-4">
-                    <div class="col-lg-3">
-                        <h6><b>Евтушенко Ю.Г. </b><br><small>академик РАН, г. Москва</small></h6>
-                        <h6><b>Садовничий В.А. </b><br><small>академик РАН, г. Москва</small></h6>
-                        <h6><b>Тыртышников Е.Е.</b><br><small>академик РАН, г. Москва</small></h6>
+                <div class="row">
+                    <div class="col-lg-3"> <h6 class="font-weight-bold">Маркелова Л.Н.</h6></div>
+                    <div class="col-lg-3"> <h6>доцент</h6></div>
+                    <div class="col-lg-6"> <h6>Кандидат исторических наук СФ БашГУ</h6></div>
+                </div>
+                <hr class="m-1">
 
-                        <h6><b>Авхадиев Ф.Г.</b><br><small>профессор, г. Казань</small></h6>
-                        <h6><b>Айда-заде К.Р.</b><br><small>чл.-корр. АН Азербайджана, Азербайджан, г. Баку</small></h6>
-                        <h6><b>Алимов Ш.А.</b><br><small>академик АН Узбекистана,г. Ташкент</small></h6>
-                        <h6><b>Васин В.В.</b><br><small>чл.-корр. РАН, г. Екатеринбург</small></h6>
-                        <h6><b>Шкаликов А.А.</b><br><small>чл.-корр. РАН,г. Москва</small></h6>
+                <div class="row">
+                    <div class="col-lg-3"> <h6 class="font-weight-bold">Бойко Н.Н.</h6></div>
+                    <div class="col-lg-3"> <h6>доцент</h6></div>
+                    <div class="col-lg-6"> <h6>Кандидат юридических наук СФ БашГУ</h6></div>
+                </div>
+                <hr class="m-1">
 
-                    </div>
-                    <div class="col-lg-3">
-                        <h6><b>Губайдуллин Д.А.</b><br><small>чл.-корр. РАН, г. Казань</small></h6>
-                        <h6><b>Гущин В.А.</b><br><small>чл.-корр. РАН, г. Москва</small></h6>
-                        <h6><b>Денисов А.М.</b><br><small>профессор, г. Москва</small></h6>
+                <div class="row">
+                    <div class="col-lg-3"> <h6 class="font-weight-bold">Косцова С.А</h6></div>
+                    <div class="col-lg-3"> </div>
+                    <div class="col-lg-6"> <h6>Старший преподаватель СФ БашГУ</h6></div>
+                </div>
+                <hr class="m-1">
 
-                        <h6><b>Елизаров А.М.</b><br><small>профессор, г. Казань</small></h6>
-                        <h6><b>Жегалов В.И.</b><br><small>профессор, г. Казань</small></h6>
-                        <h6><b>Билалов Б.Т.</b><br><small>чл.-корр. АН Азербайджана, г. Баку</small></h6>
-                        <h6><b>Кальменов Т.Ш.</b><br><small>академик НАН Казахстана, Казахстан, г. Алматы</small></h6>
+                <div class="row">
+                    <div class="col-lg-3"> <h6 class="font-weight-bold">Дмитриев В.Л.</h6></div>
+                    <div class="col-lg-3"> <h6>доцент</h6></div>
+                    <div class="col-lg-6"> <h6>Кандидат физико-математических наук СФ БашГУ</h6></div>
+                </div>
+                <hr class="m-1">
 
-                        <h6><b>Ильгамов М.А.</b><br><small>чл.-корр. РАН, г. Уфа</small></h6>
-                        <h6><b>Ильин А.В.</b><br><small>чл.-корр. РАН, г. Москва</small></h6>
+                <div class="row">
+                    <div class="col-lg-3"> <h6 class="font-weight-bold">Ягудина А.Р.</h6></div>
+                    <div class="col-lg-3"> <h6>доцент</h6></div>
+                    <div class="col-lg-6"> <h6>Кандидат социологических наук СФ БашГУ</h6></div>
+                </div>
+                <hr class="m-1">
 
-                    </div>
-                    <div class="col-lg-3">
-                        <h6><b>Кабанихин С.И.</b><br><small>чл.-корр. РАН, г. Новосибирск</small></h6>
+                <div class="row">
+                    <div class="col-lg-3"> <h6 class="font-weight-bold">Анохина В.Н.</h6></div>
+                    <div class="col-lg-3"> </div>
+                    <div class="col-lg-6"> <h6>Специалист НИУ СФ БашГУ</h6></div>
+                </div>
+                <hr class="m-1">
 
-                        <h6><b>Ломов И.С.</b><br><small>профессор, г. Москва</small></h6>
-                        <h6><b>Мейрманов А.М.</b><br><small>профессор, г. Белгород</small></h6>
-                        <h6><b>Петров И.Б.</b><br><small>чл.-корр. РАН, г. Москва</small></h6>
+                <div class="row">
+                    <div class="col-lg-3"> <h6 class="font-weight-bold">Баева О.В.</h6></div>
+                    <div class="col-lg-3"> </div>
+                    <div class="col-lg-6"> <h6>Специалист НИУ СФ БашГУ</h6></div>
+                </div>
+                <hr class="m-1">
 
-
-                        <h6><b>Прилепко А.И.</b><br><small>профессор, г. Москва</small></h6>
-                        <h6><b> Manfred Schneider</b><br><small>профессор, Германия, г. Карлсруэ</small></h6>
-                        <h6><b>Kamal Kanti Nandi</b><br><small>профессор, Индия, г. Силигори</small></h6>
-                        <h6><b>Раджабов Н.Р.</b><br><small>академик АН Таджикистана, Таджикистан, г. Душанбе</small></h6>
-
-                    </div>
-
-                    <div class="col-lg-3">
-                        <h6><b>Смирнов Ю.Г.</b><br><small>профессор,г. Пенза</small></h6>
-
-                        <h6><b>Солдатов А.П.</b><br><small>профессор,г. Москва</small></h6>
-                        <h6><b>Khanlar Mamedov</b><br><small>профессор, Турция</small></h6>
-                        <h6><b>Шабаров А.Б.</b><br><small>профессор,г. Тюмень</small></h6>
-                        <h6><b>Шагапов В.Ш.</b><br><small>академик АН РБ,г. Уфа</small></h6>
-
-                        <h6><b>Попиванов Н.</b><br><small>профессор, Болгария, г. София</small></h6>
-
-                        <h6><b>Садыбеков М.А.</b><br><small>член-корр. НАН Казахстана, Казахстан, г. Алматы</small></h6>
-
-                        <h6><b>Шананин А.А.</b><br><small>чл.-корр. РАН,г. Москва</small></h6>
-
-                        <h6><b>Ягола А.Г.</b><br><small>профессор,г. Москва</small></h6>
-
-                    </div>--}}
+                <div class="row">
+                    <div class="col-lg-3"> <h6 class="font-weight-bold">Саньяпова Л.Р.</h6></div>
+                    <div class="col-lg-3"> </div>
+                    <div class="col-lg-6"> <h6>Документовед НИУ СФ БашГУ</h6></div>
                 </div>
             </div>
         </div>
